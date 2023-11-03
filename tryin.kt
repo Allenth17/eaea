21f81a8c190ad2e06b35e8361bb7ccc3f00de278
Allenth17 <allul1712@gmail.com>
2023-11-03 07:35:17 +0700

    first commit

---
+++ tryin.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+fun main(){
+    val masuk = Scanner(System.`in`)
+    print("Namamu: ")
+    val nama = masuk.nextLine()
+    abstract fun clear()
+
+    println("Halo, $nama")
+    println("Welkam tu Cartenz")
+}
