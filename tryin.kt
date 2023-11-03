f038971f64455d67f490c6f604f53c09fb70b991
Allenth17 <allul1712@gmail.com>
2023-11-03 08:37:38 +0700

    insyaallah

--- tryin.kt
+++ tryin.kt
@@ -3,7 +3,6 @@ fun main(){
     val masuk = Scanner(System.`in`)
     print("Namamu: ")
     val nama = masuk.nextLine()
-    abstract fun clear()
 
     println("Halo, $nama")
     println("Welkam tu Cartenz")
